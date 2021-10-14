45dd754e17045f9e179f1709d8dcdc8656a764eb
Majey <kevinerick78@yahoo.com>
2021-10-14 11:31:50 +0300

    app.component update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `
-    <pm-header></pm-header>
-    <router-outlet></router-outlet>
-    <pm-footer></pm-footer>    
-  `
+  templateUrl: `./app.component.html`
 })
 export class AppComponent {
   title = 'attorney-website-angularVersion';
